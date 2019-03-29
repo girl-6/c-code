5833ead8b7a347ff1f61f24d3b1370350c99566f
angle-99 <1971561926@qq.com>
2019-03-29 21:13:13 +0800

    带多继承指针问题

--- 继承（三）/继承（三）/main.cpp
+++ 继承（三）/继承（三）/main.cpp
@@ -4,37 +4,64 @@
 #include <iostream>
 using namespace std;
 
-class Person
+  //菱形继承及菱形虚拟继承
+//class Person
+//{
+//public:
+//	string _name;
+//};
+//class Student:virtual public Person    //虚拟继承方式
+//{
+//protected:
+//	int _no;
+//};
+//class Teacher:virtual public Person   //虚拟继承方式
+//{
+//protected:
+//	int _id;
+//};
+//class Assistant :public Student, public Teacher
+//{
+//	string _course;     //主修课程
+//};
+//void Test()
+//{
+//	Assistant a;
+//	a._name = "ly";     //无virtual 关键字时，_name的二义性带来编译错误
+//
+//	//指明成员的作用域可以解决二义性，却带来了数据冗余问题
+//	a.Student::_name = "ll";
+//	a.Teacher::_name = "yy";
+//}
+
+   //多继承中的指针偏移
+class A
 {
 public:
-	string _name;
+	int _a;
 };
-class Student:virtual public Person    //虚拟继承方式
+class B
 {
-protected:
-	int _no;
+public:
+	int _b;
 };
-class Teacher:virtual public Person   //虚拟继承方式
+class C :public A, public B     //c对象成员与继承顺序有关
 {
-protected:
-	int _id;
+public:
+	int _c;
 };
-class Assistant :public Student, public Teacher
-{
-	string _course;     //主修课程
-};
-void Test()
-{
-	Assistant a;
-	a._name = "ly";     //无virtual 关键字时，_name的二义性带来编译错误
 
-	//指明成员的作用域可以解决二义性，却带来了数据冗余问题
-	a.Student::_name = "ll";
-	a.Teacher::_name = "yy";
-}
 int main()
 {
-	Test();
+	C c;
+	A *p1 = &c;
+	B *p2 = &c;
+	C *p3 = &c;
 	system("pause");
 	return 0;
 }
+
+//A.p1==p2==p3
+//B.p1<p2<p3
+//C.p1==p3!=p2    正确
+//D.p1!=p2!=p3
